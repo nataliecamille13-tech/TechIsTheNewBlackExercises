334eca4c71f4accece0de3faabb22193711b2ebf
natalie <nataliecamille13@gmail.com>
2026-09-12 16:08:02 -0400

    Adding New Exercise

---
+++ src/week2/section22/SecondsUntilNewYear.java
@@ -0,0 +1,36 @@
+package week2.section22;
+
+public class SecondsUntilNewYear {
+    public static void main(String[] args) {
+        int hour = 11;
+        int minute = 59;
+        int second = 52;
+
+        ++second;
+        System.out.println("11:59:" + second + " PM");
+
+        ++second;
+        System.out.println("11:59:" + second + " PM");
+
+        ++second;
+        System.out.println("11:59:" + second + " PM");
+
+        ++second;
+        System.out.println("11:59:" + second + " PM");
+
+        ++second;
+        System.out.println("11:59:" + second + " PM");
+
+        ++second;
+        System.out.println("11:59:" + second + " PM");
+
+        ++second;
+        System.out.println("11:59:" + second + " PM");
+
+        ++second;
+        System.out.println("12:00:00 AM");
+
+        System.out.println("Happy New Year! Let's make this year our best year yet. Let's set goals that scare us, take massive action towards them, and refuse to settle for anything less than our absolute best. Remember, success is your duty, your obligation, and your responsibility. Let's dominate our goals and make this year one for the record books. Let's do whatever it takes to make it happen!");
+    }
+
+}
