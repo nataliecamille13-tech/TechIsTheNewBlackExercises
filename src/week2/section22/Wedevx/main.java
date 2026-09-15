0a30ffdebe6a81cdcd1199022d41de23c954f8e1
natalie <nataliecamille13@gmail.com>
2026-09-15 19:48:39 -0400

    Adding New Exercise

---
+++ src/week2/section22/Wedevx/main.java
@@ -0,0 +1,29 @@
+package week2.section22.Wedevx;
+
+public class main {
+    public static void main(String[] args) {
+        int seconds = 10;
+
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+        seconds--;
+        System.out.println("Countdown: " + seconds);
+
+        System.out.println("Hooray! The IPO of wedevx.co is now live!");
+    }
+}
