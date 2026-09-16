f603952e4397fb9c63480541dee112e1de6428e0
natalie <nataliecamille13@gmail.com>
2026-09-15 20:11:33 -0400

    Adding New Exercise

---
+++ src/week2/section26/Main.java
@@ -0,0 +1,16 @@
+package week2.section26;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Please enter an int num:");
+        int num = scanner.nextInt();
+
+        if (num > 42) {
+            System.out.println("Superstar");
+        }
+    }
+}
