564b4897d92033552cd6177c73e7feade48d158c
natalie <nataliecamille13@gmail.com>
2026-09-15 20:02:21 -0400

    Adding New Exercise

---
+++ src/week2/section22/CountTo10AndBack.java
@@ -0,0 +1,29 @@
+package week2.section22;
+
+ class CountTo10AndBack {
+    public static void main(String[] args) {
+        int num = 0;
+
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num--;
+        System.out.println(num);
+        num--;
+        System.out.println(num);
+        num--;
+        System.out.println(num);
+        num--;
+        System.out.println(num);
+        num--;
+        System.out.println(num);
+    }
+}
