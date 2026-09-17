4fed3dbdd883d8763feb1ce954eee89976c8a873
natalie <nataliecamille13@gmail.com>
2026-09-16 21:35:47 -0400

    Adding New Exercise

---
+++ src/week2/section21/PrintNumbersFrom1To5.java
@@ -0,0 +1,17 @@
+package week2.section21;
+
+public class PrintNumbersFrom1To5 {
+    public static void main(String[] args) {
+        int num = 1;
+
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+        num++;
+        System.out.println(num);
+    }
+}
