d33098bd7c8462b282c898539a7cc7889feb0c7e
natalie <nataliecamille13@gmail.com>
2026-09-16 21:53:43 -0400

    Adding New Exercise

---
+++ src/week2/section19/AppleWatch.java
@@ -0,0 +1,16 @@
+package week2.section19;
+
+import java.util.Scanner;
+
+public class AppleWatch {
+    public static void main(String[] args) {
+
+        double price = 149.45;
+        double shipping = 12.50;
+
+        price *= 2;
+        price += shipping;
+
+        System.out.println(price);
+    }
+}
